01c6215d68d4b06a80751673bebe9556b39939bf
Ravi Pratap Singh <raviand26748@gmail.com>
2020-05-20 22:39:46 +0530

    just getting started

---
+++ app/src/main/java/com/example/ticketchecker/loggedin.java
@@ -0,0 +1,28 @@
+package com.example.ticketchecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+public class loggedin extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loggedin);
+        Button scan =findViewById(R.id.scan);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+}
